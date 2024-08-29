eef407a8afd245872c94912566bf0113533eda27
Abdellah Fanidi <afanidi@c4r2p3.1337.ma>
2024-08-29 22:52:40 +0100

    build ex02

--- cpp05/ex01/Form.hpp
+++ cpp05/ex01/Form.hpp
@@ -1,13 +1,51 @@
 #pragma once
+#include <iostream>
+
 
 class Form
 {
-private:
+    private:
         const std::string name;
-public:
-    Form();
-    ~Form();
-    Form::Form(const Form &b);
-    Form& operator=(const Form& b);
+        bool signe;
+        const int gradeSign;
+        const int gradeExc;
+
+    public:
+        Form();
+        ~Form();
+        Form::Form(const Form &b);
+        Form& operator=(const Form& b);
+
+    // getters :
+    std::string getName();
+    bool getSigne();
+    int getGradeSign();
+    int getGradeExc();
+
+    //exception : 
+    class GradeTooHighException : public std::exception
+    {
+        public :
+            const char* what() const throw();
+    };
+
+    class GradeTooLowException : public std::exception
+    {
+        public :
+            const char* what() const throw();
+    };
 };
 
+std::ostream& operator<<(std::ostream& os, Form b);
+
+
+std::ostream& operator<<(std::ostream& os, Form b)
+{
+    os << "FOrm -> name: " << b.getName();
+    if (b.getSigne())
+        os << " , is signed";
+    else    
+        os << " , is not signed";
+    os << " , grade required to sign it: "<<b.getGradeSign();
+    os << " , grade required to execute it: "<<b.getGradeExc()<<std::endl;
+}
