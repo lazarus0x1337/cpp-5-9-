4b79f5b2291b2ff805bc236e7dd47473f545fdb5
Abdellah Fanidi <afanidi@c3r6p5.1337.ma>
2024-09-16 20:39:50 +0100

    done ex00

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -19,74 +19,146 @@ ScalarConverter& ScalarConverter::operator=(const ScalarConverter &b)
     return (*this);
 }
 
+int stringToInt(const std::string& input)
+{
+    std::istringstream iss(input);
+    int number;
+    iss >> number;
+    return number;
+}
+
+float stringToFloat(const std::string& input)
+{
+    std::istringstream iss(input);
+    float number;
+    iss >> number;
+    return number;
+}
+
+double stringToDouble(const std::string& input)
+{
+    std::istringstream iss(input);
+    double number;
+    iss >> number;
+    return number;
+}
+
 bool is_number(std::string str)
 {
-    int i = 0;
+    unsigned int i = 0;
+    int flag_dot = 0;
+    int pos_dot  = -1;
     if (str[i] == '+' || str[i] == '-')
         i++;
-    for (; i < str.size() - 1; i++)
-        if (!isdigit(str[i]))
+    for (; i < str.size(); i++)
+    {
+        if (str[i] == 'f' && i != str.size() - 1)
             return false;
-    if (str.back() != 'f' && !isdigit(str[i]))
-        return false;
+        if (str[i] == '.')
+        {
+            flag_dot++;
+            pos_dot = i;
+            continue;
+        }
+        if (!isdigit(str[i]) && str[i] != 'f')
+            return false;
+    }
+    if (pos_dot != -1)
+    {
+        if (flag_dot != 1)
+            return false;
+        if (pos_dot == 0 || pos_dot == (int)(str.size() - 1))
+            return false;
+        else if (str[pos_dot + 1] == 'f')
+            return false;
+    }
     return true;        
 }
 
+int handle_char(std::string str)
+{
+    int num;
+    std::cout << "char: ";
+    if (str.size() == 1 && isalpha(str[0]))
+    {
+        std::cout << "\'"<< str[0]<<"\'"<<std::endl;
+        return (2);
+    }  
+    if (is_number(str))
+    {
+        num = stringToInt(str);
+        if (num >= 32 && num <= 126)
+        {
+            std::cout << "\'"<< (char)num<<"\'"<<std::endl;
+            return (1);
+        }
+        else
+            std::cout << "Non displayable"<<std::endl;
+        return (1);
+    }
+    else
+        std::cout << "impossible"<<std::endl;
+    return (0);
+}
+
+void handle_int(int typ, std::string str)
+{
+    std::cout << "int: ";
+    if (!typ)
+        std::cout << "impossible"<<std::endl;
+    else if (typ == 1)
+        std::cout << stringToInt(str)<<std::endl;
+    else
+        std::cout << (int)str[0]<<std::endl;
+}
+
+void handle_irrational(int type,std::string type_literal, std::string str)
+{
+    
+    std::cout << type_literal<<": ";
+    if (!type)
+    {
+        if (type_literal[0] == 'f')
+        {
+            if (str == "-inf" || str == "-inff")
+                std::cout << "-inff"<<std::endl;
+            else if (str == "+inf" || str == "+inff" || str == "inf" || str == "inff")
+                std::cout << "+inff"<<std::endl;
+            else    
+                std::cout << "nanf"<<std::endl;
+        
+        }
+        else
+        {
+            if (str == "-inf" || str == "-inff")
+                std::cout << "-inf"<<std::endl;
+            else if (str == "+inf" || str == "+inff" || str == "inf" || str == "inff")
+                std::cout << "+inf"<<std::endl;
+            else    
+                std::cout << "nan"<<std::endl;
+        }
+    }
+    else if (type == 2)
+    {
+        if (type_literal[0] == 'f')
+            std::cout << (float)str[0]<<"f"<<std::endl;
+        else
+            std::cout << (double)str[0]<<"f"<<std::endl;
+    }
+    else
+    {
+        if (type_literal[0] == 'f')
+            std::cout <<stringToFloat(str)<<"f"<<std::endl;
+        else
+            std::cout << stringToDouble(str)<<std::endl;
+    }
+}
 
 void ScalarConverter::convert(const std::string literal)
 {
-    // handling char :
-    std::cout << "char: ";
-    // if(is_number(literal))
-    // {
-    //     int num = stringToInt(literal,is_number(literal));
-    //     if (num >= 32 && num <= 126)
-    //        std::cout << "\'"<< (char)num<<"\'";
-    //     else
-    //         std::cout << "Non displayable"<<std::endl;
-    // }
-    // else
-    //     std::cout << " "<<std::endl; 
+    int type = handle_char(literal);
+    handle_int(type,literal);
+    handle_irrational(type,"float",literal);
+    handle_irrational(type,"double",literal);
 }
-
-
-
-// int stringToInt(const std::string& str, int flag)
-// {
-//     int num_exctracting;
-//     if (flag == 1)
-//         return (int)str[0];
-//     std::istringstream converting(str);
-//     converting >> num_exctracting;
-//     return num_exctracting;
-// }
-
-// int is_number(std::string literal)
-// {
-//     //case for one caracter :
-//     if (literal.size() == 1 && !isdigit(literal[0]))
-//         return 1;
-//     unsigned int i = 0;
-//     int flag = 0;
-//     if (literal[i] =='-' || literal[i] == '+')
-//         i++;
-//     for( ; i < literal.length() ; i++)
-//     {
-//         if (!isdigit(literal[i]))
-//             return 0;
-//         flag = 1;
-//     }
-//     if (flag)
-//         return 2;
-//     return 0;
-// }
-
-
-// int stringToInt(const std::string& str)
-// {
-//     int num_exctracting;
-//     std::istringstream converting(str);
-//     converting >> num_exctracting;
-//     return num_exctracting;
-// }
 
--- cpp06/ex00/istring.cpp
+++ cpp06/ex00/istring.cpp
@@ -2,9 +2,9 @@
 #include <sstream>  // For std::istringstream
 #include <string>
 
-int stringToInt(const std::string& input) {
+float stringToInt(const std::string& input) {
     std::istringstream iss(input);  // Create an input string stream
-    int number;
+    float number;
     iss >> number;  // Extract the integer from the stream
     if (iss.fail()) {
         throw std::invalid_argument("Invalid input: not a valid integer");
@@ -18,7 +18,7 @@ int main(int ac, char **av) {
         // need to check for caracter alpha :  just we need string.back()=='f'
 
         // requirement :  + or - for begine ,  just digit , and the lase one have f or digits!!
-        int number = stringToInt(input);
+        float number = stringToInt(input);
         // same thing for float and double !!
         std::cout << "Converted number: " << number << std::endl;
     } catch (const std::invalid_argument& e) {
